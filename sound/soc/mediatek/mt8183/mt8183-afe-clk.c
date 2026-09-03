30cf48383006579d7cd35380cc6e766260b72bff
bui duc phuc <phucduc.bui@gmail.com>
2026-09-03 11:52:01 +0700

    ASoC: mediatek: mt8183: Fix APLL enable error handling
    Currently, the mt8183_apll*_enable() functions call mux_setting(afe, true)
    but do not check its return value to handle failures.
    
    In addition, the cleanup paths of mt8183_apll*_enable() do not call
    mux_setting(afe, false) when the enable operation fails, while the
    mt8183_apll*_disable() functions do.
    
    Add error handling for apll*_mux_setting() and call mux_setting(afe, false)
    in the cleanup paths when mt8183_apll*_enable() fails.
    
    Fixes: a94aec035a12 ("ASoC: mediatek: mt8183: add platform driver")
    Reviewed-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Signed-off-by: bui duc phuc <phucduc.bui@gmail.com>
    Link: https://patch.msgid.link/20260903045203.175337-4-phucduc.bui@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt8183/mt8183-afe-clk.c
+++ sound/soc/mediatek/mt8183/mt8183-afe-clk.c
@@ -376,7 +376,9 @@ int mt8183_apll1_enable(struct mtk_base_afe *afe)
 	int ret;
 
 	/* setting for APLL */
-	apll1_mux_setting(afe, true);
+	ret = apll1_mux_setting(afe, true);
+	if (ret)
+		return ret;
 
 	ret = clk_prepare_enable(afe_priv->clk[CLK_APLL22M]);
 	if (ret) {
@@ -405,6 +407,8 @@ int mt8183_apll1_enable(struct mtk_base_afe *afe)
 ERR_CLK_APLL1_TUNER:
 	clk_disable_unprepare(afe_priv->clk[CLK_APLL22M]);
 ERR_CLK_APLL22M:
+	apll1_mux_setting(afe, false);
+
 	return ret;
 }
 
@@ -430,7 +434,9 @@ int mt8183_apll2_enable(struct mtk_base_afe *afe)
 	int ret;
 
 	/* setting for APLL */
-	apll2_mux_setting(afe, true);
+	ret = apll2_mux_setting(afe, true);
+	if (ret)
+		return ret;
 
 	ret = clk_prepare_enable(afe_priv->clk[CLK_APLL24M]);
 	if (ret) {
@@ -459,6 +465,8 @@ int mt8183_apll2_enable(struct mtk_base_afe *afe)
 ERR_CLK_APLL2_TUNER:
 	clk_disable_unprepare(afe_priv->clk[CLK_APLL24M]);
 ERR_CLK_APLL24M:
+	apll2_mux_setting(afe, false);
+
 	return ret;
 }
 
